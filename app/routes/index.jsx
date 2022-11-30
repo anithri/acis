ff48fab63ada2fe4ec69e12efcfd003d60f5d3a4
Scott M Parrish <anithri@gmail.com>
2022-11-29 19:05:04 -0700

    initial commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,9 @@
+import { Link } from "@remix-run/react";
+
+export default function Index() {
+  return (
+    <div className="">
+      <h1 className="text-2xl font-bold">Welcome to Remix</h1>
+    </div>
+  );
+}
